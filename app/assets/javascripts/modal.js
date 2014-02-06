cf8797b4f84b3f0d508e65718d7c318754ddaf19
Simon Taranto <simon.taranto@gmail.com>
2014-02-06 16:11:24 -0700

    bring JS up to date with API updates
    fix js bugs in modal.js
    
    compile assets to fix bug
    
    fix more bugs and precompile

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,14 +1,15 @@
 var Modal = {};
 Modal.addModalListener = function(payment, products, market_data, val){
-  for( i = 0; i < market_data[0].payment_types.length; i++) {
-    payment.push(val.payment_types[i].name);
+  var market = market_data.market
+  for( i = 0; i < market.payment_types.length; i++) {
+    payment.push(val.payment_types[i]);
   };
 
-  for( i = 0; i < market_data[0].products.length; i++) {
-    products.push(val.products[i].name);
+  for( i = 0; i < market.products.length; i++) {
+    products.push(val.products[i]);
   };
-   var html = '<h3 class="name">' + market_data[0].name + '</h3>' + '<div><hr>' +
-'<div class="address">' + market_data[0].address.street + '</br>' + market_data[0].address.city + ', ' + market_data[0].address.state + '</div>' +
+   var html = '<h3 class="name">' + market.name + '</h3>' + '<div><hr>' +
+'<div class="address">' + market.address.street + '</br>' + market.address.city + ', ' + market.address.state + '</div>' +
 '<div class="payments"><img src="assets/payment.png">' + payment.join(' ✤ ') + '<img src="assets/payment.png"></div>' + '<div class="products"><img src="assets/product.png">' + products.join(' ✿  ') + '<img src="assets/product.png"></div>';
 
   $(".md-content").html(html);
