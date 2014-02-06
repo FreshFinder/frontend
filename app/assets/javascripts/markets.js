fa6cff1b874afd3c1b1e04ee19a4ddff8481d2ad
Katrina Engelsted <katrina@engelsted.co>
2014-02-06 14:14:02 -0700

    taking out the hard coded localhost

--- app/assets/javascripts/markets.js
+++ app/assets/javascripts/markets.js
@@ -8,7 +8,7 @@ $(".modal-close").click(function (e){
   $('#modal').hide();
   });
 
-var marketData = "http://localhost:5555/api/v1/markets.json?address=true";
+var marketData = "/api/v1/markets.json?address=true";
 
 $.getJSON( marketData, function( data ) {
   $.each(data, function(index, val) {
@@ -32,7 +32,7 @@ $.getJSON( marketData, function( data ) {
 
         $('.market-item').click(function(el){
           var marketId = $(this).data("market-id"),
-              url = "http://localhost:5555/api/v1/markets/" + marketId;
+              url = "/api/v1/markets/" + marketId;
 
           $.getJSON( url, function( market_data ) {
             $.each(market_data, function (i,val) {
