1de12d0ff9e42043e073f93238c0f1d933e10226
Simon Taranto <simon.taranto@gmail.com>
2014-02-10 14:35:56 -0700

    open sidebar upon search and add listener to enter key in input field

--- app/assets/javascripts/marketSearch.js
+++ app/assets/javascripts/marketSearch.js
@@ -1,8 +1,11 @@
 jQuery(document).ready(function() {
   var Search = {};
 
-  $("#search-button").click(function (e) {
-    // open the sidebar
+  Search.performSearch = function() {
+    var container = $('.st-container');
+    container.addClass('st-menu-open');
+    container.addClass('st-effect-2');
+
     Search.searchInput = $("#search-input").val();
     Search.route = "/api/v1/search/markets?zipcode=" + Search.searchInput;
     $.getJSON( Search.route, function(data) {
@@ -12,6 +15,17 @@ jQuery(document).ready(function() {
         $(list).append('<li><a class="icon icon-data market-item" data-market-id=' + market.id + '>' + market.name + '<p class="smaller"> </p>' + '</a></li>');
       });
     });
+  };
+
+  $("#search-button").click(function (e) {
+    Search.performSearch();
   });
+
+  $("#search-input").keypress(function (e) {
+    if (e.keyCode == 13) {
+      Search.performSearch();
+    };
+  });
+
 });
 
