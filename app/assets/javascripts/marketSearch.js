f2186cc25786d206bf2a13daeb3ec861cd887c6e
Simon Taranto <simon.taranto@gmail.com>
2014-02-10 14:10:13 -0700

    make search button populate sidebar (without opening)

--- app/assets/javascripts/marketSearch.js
+++ app/assets/javascripts/marketSearch.js
@@ -2,8 +2,16 @@ jQuery(document).ready(function() {
   var Search = {};
 
   $("#search-button").click(function (e) {
-    console.log("foobar, arnold");
+    // open the sidebar
+    Search.searchInput = $("#search-input").val();
+    Search.route = "/api/v1/search/markets?zipcode=" + Search.searchInput;
+    $.getJSON( Search.route, function(data) {
+      var list = $(".listings");
+      list.empty();
+      $.each(data, function(_, market){
+        $(list).append('<li><a class="icon icon-data market-item" data-market-id=' + market.id + '>' + market.name + '<p class="smaller"> </p>' + '</a></li>');
+      });
+    });
   });
-
 });
 
