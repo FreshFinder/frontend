0b4479971722fdd4145b0994ecab91534771de4f
Katrina Engelsted <katrina@engelsted.co>
2014-02-11 10:25:33 -0700

    working on drafting show page

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -3,29 +3,8 @@ class Schedule
   attr_reader :day, :start_time, :end_time
 
   def initialize(market_schedule)
-    @day = market_schedule["day"]
-    # @day = market_schedule[weekday(day)]
-    @start_time = market_schedule["start_time"]
-    @end_time = market_schedule["end_time"]
+    @day = Date::DAYNAMES[market_schedule["day"]]
+    @start_time = Time.parse(market_schedule["start_time"]).strftime("%I:00")
+    @end_time = Time.parse(market_schedule["end_time"]).strftime("%I:00pm")
   end
-
-  #  def weekday[day]
-  #   case day
-  #   when  0
-  #     Sunday
-  #   when 1
-  #     Monday
-  #   when  2
-  #     Tuesday
-  #   when 3
-  #     'Wednesday'
-  #   when 4
-  #     'Thursday'
-  #   when 5
-  #     'Friday'
-  #   when 6
-  #     'Saturday'
-  #   end
-  # end
-
 end
