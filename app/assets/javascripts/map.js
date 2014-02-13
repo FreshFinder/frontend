71108cbcb3811ed5c97b00f19094c85634d251a6
Katrina Engelsted <katrina@engelsted.co>
2014-02-12 23:17:53 -0700

    WIP popups styling

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,27 +2,27 @@ var Map = {};
 
 Map.addMarkerToLayer = function(val) {
   var lng =  parseFloat(val.address.long),
-      lat =  parseFloat(val.address.lat),
-      name = val.name,
-      id = val.id,
-      markerLayer = L.mapbox.markerLayer({
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [lng , lat]
-        },
-        properties: {
-          market_id: id,
-          name: name,
-          description: val.address.description,
-          street: val.address.street,
-          city: val.address.city,
-          name: val.name,
-          'marker-size': 'medium',
-          'marker-color': '#2ca25f',
-          'marker-symbol': 'park2'
-        }
-      })
+  lat =  parseFloat(val.address.lat),
+  name = val.name,
+  id = val.id,
+  markerLayer = L.mapbox.markerLayer({
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [lng , lat]
+    },
+    properties: {
+      market_id: id,
+      name: name,
+      description: val.address.description,
+      street: val.address.street,
+      city: val.address.city,
+      state: val.address.state,
+      'marker-size': 'medium',
+      'marker-color': '#2ca25f',
+      'marker-symbol': 'park2'
+    }
+  })
   return markerLayer;
 };
 
@@ -32,16 +32,45 @@ Map.addPopupToLayer = function(layer) {
   var addressDescription = market.description
   var street = market.street
   var city = market.city
+  var state = market.state
 
-  if (addressDescription == null || addressDescription == 'Other' || street == null) {
-    addressDescription = "",
-    street = ""
-  } else {
-    addressDescription = '<em>Directions: ' + addressDescription + '</em>',
-    street = street + ',  '
-  }
-  
-  var content = '<img class="wheat"/><div class="main-info"><h4 class="namer"><strong>'+ "<a href='/markets/" + market.market_id + "'" + '>' + market.name + '</a>' + '</strong></h4>' + '<p class="addressy">' + city + '</br>' + addressDescription + '</div>';
+  if (city === null ) {city = ""}
+
+  switch (state) {
+    case null :
+      state = "";
+      break;
+    case city !== null && state !== null :
+      state = ',  ' + state ;
+      break;
+    default:
+      state = state;
+    };
+
+  addressDescription = processAddress(addressDescription);
+  street = processStreet(street);
+
+  var content = '<img class="wheat"/><div class="main-info"><h4 class="namer"><strong>'+ "<a href='/markets/" + market.market_id + "'" + '>' + market.name + '</a>' + '</strong></h4>' + '<span class="addressy">' + city + ',  ' + state + '</span></br><span class="location">' + addressDescription + '</span></div>';
   
   return content;
 }
+
+var processAddress = function(addressDescription){
+  if (addressDescription === null || addressDescription === 'Other') {
+    addressDescription = ""
+  } else {
+    addressDescription = 'Location: ' + addressDescription;
+  };
+  return addressDescription;
+};
+
+var processStreet = function(street){
+  if (street === null ){ 
+    street = "" 
+  } else {
+    street = street + ',  ';
+  };
+  return processStreet;
+};
+
+
